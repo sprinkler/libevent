871a6549f98fe2d4e1814c12eafc6de10d0d4ef7
Nicu Pavel <npavel@mini-box.com>
2015-05-05 09:26:19 +0300

    Fix compilation and symbol export. Default as shared library

--- Android.mk
+++ Android.mk
@@ -6,13 +6,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE:= libevent2
-LOCAL_MODULE_TAGS:= optional
+LOCAL_MODULE_TAGS:= eng debug
 
 LOCAL_SRC_FILES := \
     buffer.c \
     bufferevent.c \
     bufferevent_filter.c \
-    bufferevent_openssl.c \
     bufferevent_pair.c \
     bufferevent_ratelim.c \
     bufferevent_sock.c \
@@ -41,7 +40,7 @@ LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/include \
     external/openssl/include
 
-LOCAL_CFLAGS := -DHAVE_CONFIG_H -DANDROID -fvisibility=hidden
+LOCAL_CFLAGS := -DHAVE_CONFIG_H -DANDROID
 
-include $(BUILD_STATIC_LIBRARY)
-#include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
